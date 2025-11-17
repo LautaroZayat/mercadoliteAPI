de1cf8c47e10f4c54dad283b93e6be2c97496fb1
Iara Genco <148562678+yarisuli@users.noreply.github.com>
2025-11-16 23:39:29 -0300

    aca no paso nada

--- index.js
+++ index.js
@@ -40,9 +40,7 @@ app.get('/', (req, res) => {
   res.send('MercadoLiteAPI funcionando correctamente!');
 });
 
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () =>
-//   console.log(`🚀 API escuchando en http://localhost:${PORT}`)
-// );
-
-export default app;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>
+  console.log(`🚀 API escuchando en http://localhost:${PORT}`)
+);
